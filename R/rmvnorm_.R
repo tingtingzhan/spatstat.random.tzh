6ba20dc69727fe63ea597ca889f44137050723c4
Tingting Zhan <tingtingzhan@gmail.com>
2024-12-13 21:23:26 -0500

    test

--- R/rmvnorm_.R
+++ R/rmvnorm_.R
@@ -5,17 +5,28 @@
 
 #' @title Batch Process of Random Multivariate Normal Generation
 #' 
+#' @description
+#' Batch process of random multivariate normal generation.
+#' 
 #' @param n \link[base]{integer} scalar
 #' 
-#' @param mu \link[base]{list} or \link[base]{data.frame}. 
-#' Each element must be a \link[base]{numeric} scalar or \link[base]{vector}
+#' @param mu \link[base]{list} or \link[base]{data.frame},
+#' each element must be a \link[base]{numeric} scalar or \link[base]{vector},
+#' used as the multivariate means \eqn{\mathbf{\mu}}'s
 #' 
-#' @param Sigma \link[base]{list}.
-#' Each element must be \link[base]{numeric}, either 
+#' @param Sigma \link[base]{list}, 
+#' each element must be \link[base]{numeric}, either 
 #' a scalar, a \link[base]{vector} or a \link[base]{matrix}, 
-#' to be used as the \link[stats]{var}iance-\link[stats]{cov}ariance matrices.
+#' used as the \link[stats]{var}iance-\link[stats]{cov}ariance 
+#' \link[base]{matrix}es \eqn{\Sigma}'s
 #' 
 #' @param ... additional parameters, currently not in use
+#' 
+#' @returns
+#' Function [rmvnorm_] returns a \link[base]{list} of \link[base]{matrix}es.
+#' 
+#' @note
+#' Workhorse function \link[MASS]{mvrnorm} from package \CRANpkg{MASS} is faster than `?mvtnorm::rmvnorm`.
 #' 
 #' @examples
 #' rmvnorm_(n = 5L, mu = list(a = c(0, 3), b = c(3, 7)), Sigma = list(a = .5, b = 1.2))
